2a4c2c11438c3f45a8663d353e5879e7fdb8ca92
Renatha Victor <renatha.nvictor@gmail.com>
2021-04-09 17:15:30 -0300

    test deploy

--- pages/__tests__/login.test.js
+++ pages/__tests__/login.test.js
@@ -4,13 +4,13 @@ import { shallow } from 'enzyme'
 import SignInPage from '../login'
 import { findByTestAttr } from '../../utils/testUtils'
 
-// describe('Page - Login page', () => {
-//   let wrapper
-//   beforeEach(() => {
-//     wrapper = shallow(<SignInPage />)
-//   })
-//   test('Render page without crash', () => {
-//     let component = findByTestAttr(wrapper, 'login-page')
-//     expect(component.length).toBe(1)
-//   })
-// })
+describe('Page - Login page', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(<SignInPage />)
+  })
+  test('Render page without crash', () => {
+    let component = findByTestAttr(wrapper, 'login-page')
+    expect(component.length).toBe(1)
+  })
+})
